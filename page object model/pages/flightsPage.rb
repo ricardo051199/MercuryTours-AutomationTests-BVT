4c8042612d4f2abeb580203dd9c7aa71e6fde637
Ricardo <jesus.pelaez@ucb.edu.bo>
2023-06-14 17:39:41 -0600

    third commit

--- page object model/pages/flightsPage.rb
+++ page object model/pages/flightsPage.rb
@@ -1,15 +1,5 @@
 class FlightsPage
   include Capybara::DSL
 
-  def enter_email(email)
-    fill_in 'userName', with: email
-  end
-
-  def enter_password(password)
-    fill_in 'password', with: password
-  end
-
-  def verify_login_success_message
-    expect(page).to have_content('Login Successfully')
-  end
+  
 end
